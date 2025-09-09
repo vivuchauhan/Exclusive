80ebe6f7d16129d0f4e077240d04be953abce756
vivek chauhan <vivuchauhan333@gmail.com>
2025-09-09 17:18:16 +0530

    new update

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -52,118 +52,124 @@ function Cart() {
   return (
       <>
         <div className='container-fluid'>
-          <div className='container cartContainer'>
-            <div className='row my-3 me-auto cartLeftCont'>
-              <div className='container'><h4 className='my-2 cartPgHeading'>Cart Products</h4></div>
-              <div className='d-lg-flex justify-content-center'>
-                {cart.length !== 0 ?  (<ul className='CartContStyle ps-0'>
-                  {cart.map((item) => (
-                    <div
-                      className='d-flex my-3 CarTStyle'
-                      key={item.id}
-                    >
-                      <div  style={{ width: '10%'}}>
-                        <img
-                          src={item.thumbnail}
-                          style={{ width: '100%', height: 'auto' }}
-                          alt={item.title}
-                        />
-                      </div>
-                      <div style={{display:"flex", gap:"10%", width:"90%"}}>
-                        <div style={{ width:"50%"}}>
-                          <p
-                            className=' p-0 ps-3 m-0'
-                            style={{ fontSize: '15px', fontWeight: '600'}}
-                          >
-                            {item.title}
-                          </p>
-                          <p
-                            className='homeCardText ps-3 pb-2 mt-2'
-                            style={{ fontSize: '14px', fontWeight: '500' }}
-                          >
-                            Rating: <span style={{color:"#fc530a"}}>{item.rating}</span>
-                          </p>
+          <div className='container'>
+            <div className="row">
+              <div className='col-md-8'>
+                <div className='my-3 me-auto '>
+                <div className='container'><h4 className='my-2 cartPgHeading'>Cart Products</h4></div>
+                <div className='d-lg-flex justify-content-center'>
+                  {cart.length !== 0 ?  (<ul className='CartContStyle ps-0 d-flex justify-content-start align-items-start'>
+                    {cart.map((item) => (
+                      <div
+                        className='d-flex my-3 CarTStyle'
+                        key={item.id}
+                      >
+                        <div  style={{ width: '10%'}}>
+                          <img
+                            src={item.thumbnail}
+                            style={{ width: '100%', height: 'auto' }}
+                            alt={item.title}
+                          />
                         </div>
-                        <div style={{display:"flex", flexDirection:"column", width:"50%"}}>
-                          <div style={{display:"flex", flexDirection:"row", gap:"50px", width:"100%"}}>
+                        <div style={{display:"flex", gap:"10%", width:"90%"}}>
+                          <div style={{ width:"50%"}}>
                             <p
-                              className='homeCardText me-auto ps-lg-4 p-0 m-0'
-                              style={{ fontSize: '16px', fontWeight: '600' }}
+                              className=' p-0 ps-3 m-0'
+                              style={{ fontSize: '15px', fontWeight: '600'}}
                             >
-                              <div style={{display:"flex", flexDirection:"column"}}>
-                                <span>Price</span>
-                                <span className='text-success mt-lg-2'>₹ {item.price}</span>
-                              </div>
+                              {item.title}
                             </p>
                             <p
-                              className='homeCardText pb-2 pe-lg-5 m-0'
-                              style={{ fontSize: '14px', fontWeight: '400' }}
+                              className='homeCardText ps-3 pb-2 mt-2'
+                              style={{ fontSize: '14px', fontWeight: '500' }}
                             >
-                              <div>
-                                <span style={{ fontSize: '14px', fontWeight: '600' }}>Quantity</span>
-                                <div className='mt-2'>
-                                  <button onClick={() => QuantityDec(item.id)} className='border-0 me-lg-2'>
-                                    -
-                                  </button>
-                                  {item.quantity}
-                                  <button onClick={() => QuantityInc(item.id)} className='border-0 ms-lg-2'>
-                                    +
-                                  </button>
+                              Rating: <span style={{color:"#fc530a"}}>{item.rating}</span>
+                            </p>
+                          </div>
+                          <div style={{display:"flex", flexDirection:"column", width:"50%"}}>
+                            <div style={{display:"flex", flexDirection:"row", gap:"50px", width:"100%"}}>
+                              <p
+                                className='homeCardText me-auto ps-lg-4 p-0 m-0'
+                                style={{ fontSize: '16px', fontWeight: '600' }}
+                              >
+                                <div style={{display:"flex", flexDirection:"column"}}>
+                                  <span>Price</span>
+                                  <span className='text-success mt-lg-2'>₹ {item.price}</span>
                                 </div>
-                              </div>
-                            </p>
-                          </div>
-                          <div className='text-center mt-3'>
-                            <button
-                              className='CartRemoveBtn'
-                              size='small'
-                              style={{ fontSize: '15px', width:"100%" }}
-                              onClick={() => handleRemoveFromCart(item.id)}
-                            >
-                              Remove
-                            </button>
+                              </p>
+                              <p
+                                className='homeCardText pb-2 pe-lg-5 m-0'
+                                style={{ fontSize: '14px', fontWeight: '400' }}
+                              >
+                                <div>
+                                  <span style={{ fontSize: '14px', fontWeight: '600' }}>Quantity</span>
+                                  <div className='mt-2'>
+                                    <button onClick={() => QuantityDec(item.id)} className='border-0 me-lg-2'>
+                                      -
+                                    </button>
+                                    {item.quantity}
+                                    <button onClick={() => QuantityInc(item.id)} className='border-0 ms-lg-2'>
+                                      +
+                                    </button>
+                                  </div>
+                                </div>
+                              </p>
+                            </div>
+                            <div className='text-center mt-3'>
+                              <button
+                                className='CartRemoveBtn'
+                                size='small'
+                                style={{ fontSize: '15px', width:"100%" }}
+                                onClick={() => handleRemoveFromCart(item.id)}
+                              >
+                                Remove
+                              </button>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
-                  </ul> 
-                  ):(
-                    <div className='cartContainer'>
-                      <div className='text-center my-3 me-auto'>
-                        <img src='https://mir-s3-cdn-cf.behance.net/projects/404/95974e121862329.Y3JvcCw5MjIsNzIxLDAsMTM5.png' alt='emptyCatImg' className='img-fluid' />
+                    ))}
+                    </ul> 
+                    ):(
+                      <div className='cartContainer'>
+                        <div className='text-center my-3 me-auto'>
+                          <img src='https://mir-s3-cdn-cf.behance.net/projects/404/95974e121862329.Y3JvcCw5MjIsNzIxLDAsMTM5.png' alt='emptyCatImg' className='img-fluid' />
+                        </div>
                       </div>
-                    </div>
-                  )
-                }
+                    )
+                  }
+                </div>
+                </div>
               </div>
-            </div>
-            <div className='row AmountCarTStyle'>
-              <h4>Cart Total</h4>
-              <p>Quantity: {cart.reduce((total, item) => total + item.quantity, 0)}</p>
-              <p style={{borderBottom:"1px solid #D6D5D5", fontWeight:"600"}} className='pb-3'>Subtotal: ₹{totalAmount.toFixed(2)}</p>
-              <p style={{fontWeight:"600", color:"#777c87", fontSize:"17px"}}>Discount: ₹{discount}</p>
-              <p style={{borderBottom:"1px solid #D6D5D5"}} className='pb-3'>Shipping: <span className='ps-2 text-success' style={{font:"14px", fontWeight:"400"}}>Free</span></p>
-              <h5>Total: ₹{(totalAmount - discount).toFixed(2)} /-</h5>
-              {cart.length !== 0 ? (
-                <Link to="/checkout" className='text-center mt-3'>
-                  <button
-                    className='CartRemoveBtn'
-                    style={{ fontSize:'22px', width:"100%", height:"38px", fontWeight:"600" }}
-                  >
-                    Procees to checkout
-                  </button>
-                </Link>
-              ):(
-                <Link to="/" className='text-center mt-3'>
-                  <button
-                    className='CartRemoveBtn'
-                    style={{ fontSize:'22px', width:"100%", height:"38px", fontWeight:"600" }}
-                  >
-                    Procees to checkout
-                  </button>
-                </Link>
-              )}
+              <div className='col-md-4'>
+                <div className='px-3 AmountCarTStyle'>
+                  <h4>Cart Total</h4>
+                  <p>Quantity: {cart.reduce((total, item) => total + item.quantity, 0)}</p>
+                  <p style={{borderBottom:"1px solid #D6D5D5", fontWeight:"600"}} className='pb-3'>Subtotal: ₹{totalAmount.toFixed(2)}</p>
+                  <p style={{fontWeight:"600", color:"#777c87", fontSize:"17px"}}>Discount: ₹{discount}</p>
+                  <p style={{borderBottom:"1px solid #D6D5D5"}} className='pb-3'>Shipping: <span className='ps-2 text-success' style={{font:"14px", fontWeight:"400"}}>Free</span></p>
+                  <h5>Total: ₹{(totalAmount - discount).toFixed(2)} /-</h5>
+                  {cart.length !== 0 ? (
+                    <Link to="/checkout" className='text-center mt-3'>
+                      <button
+                        className='CartRemoveBtn'
+                        style={{ fontSize:'22px', width:"100%", height:"38px", fontWeight:"600" }}
+                      >
+                        Procees to checkout
+                      </button>
+                    </Link>
+                  ):(
+                    <Link to="/" className='text-center mt-3'>
+                      <button
+                        className='CartRemoveBtn'
+                        style={{ fontSize:'22px', width:"100%", height:"38px", fontWeight:"600" }}
+                      >
+                        Procees to checkout
+                      </button>
+                    </Link>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
